cbe5ff4a0ef950da5f544b678201e4540610258d
Iron man <ak7264482@gmail.com>
2025-03-31 01:52:17 +0530

    Fix Code

--- 11_Majority_Element/03_MajorityElement_BrutForce.cpp
+++ 11_Majority_Element/03_MajorityElement_BrutForce.cpp
@@ -10,25 +10,25 @@ int majorityElement(vector<int> &nums)
     int freq = 1, ans = nums[0];
     for (int i = 1; i < n; i++)
     {
-        if (nums[i] == ans)
+        if (freq == 0)
+        {
+            ans = nums[i];
+        }
+        if (ans == nums[i])
         {
             freq++;
         }
+
         else
         {
-            freq = 1;
-            ans = nums[i];
-        }
-        if (freq > n / 2)
-        {
-            return ans;
+            freq--;
         }
     }
     return ans;
 }
 int main()
 {
-    vector<int> nums = {3,2,3};
+    vector<int> nums = {3, 2, 3};
     int result = majorityElement(nums);
     cout << result;
     return 0;
